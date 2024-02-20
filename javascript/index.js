71889d2181fe395fd21bd06430f4437ed17e106a
tylynnhardy <159942806+tylynnhardy@users.noreply.github.com>
2024-02-20 14:25:49 -0800

    corrected js date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,27 @@
 function displayTime() {
   let phoenixElement = document.querySelector("#phoenix");
-  let phoenixDateElement = phoenixElement.querySelector(".date");
-  let phoenixTimeElement = phoenixElement.querySelector(".time");
-  let phoenixTime = moment().tz("America/Phoenix");
+  if (phoenixElement) {
+    let phoenixDateElement = phoenixElement.querySelector(".date");
+    let phoenixTimeElement = phoenixElement.querySelector(".time");
+    let phoenixTime = moment().tz("America/Phoenix");
 
-  phoenixDateElement.innerHTML = phoenixTime.format("MMMM Do YYYY");
-  phoenixTimeElement.innerHTML = phoenixTime.format(
-    "h:mm:ss [<small>a</small>]"
-  );
+    phoenixDateElement.innerHTML = phoenixTime.format("MMMM	Do YYYY");
+    phoenixTimeElement.innerHTML = phoenixTime.format(
+      "h:mm:ss [<small>]a[</small>]"
+    );
+  }
 
   let puertoRicoElement = document.querySelector("#puerto-rico");
-  let puertoRicoDate = puertoRicoElement.querySelector(".date");
-  let puertoRicoTimeElement = puertoRicoElement.querySelector(".time");
-  let puertoRicoTime = moment().tz("America/Puerto_Rico");
+  if (puertoRicoElement) {
+    let puertoRicoDateElement = puertoRicoElement.querySelector(".date");
+    let puertoRicoTimeElement = puertoRicoElement.querySelector(".time");
+    let puertoRicoTime = moment().tz("America/Puerto_Rico");
 
-  puertoRicoDate.innerHTML = puertoRicoTime.format("MMMM Do YYYY");
-  puertoRicoTimeElement.innerHTML = puertoRicoTime.format(
-    "h:mm:ss [<small>a</small>]"
-  );
+    puertoRicoDateElement.innerHTML = puertoRicoTime.format("MMMM	Do YYYY");
+    puertoRicoTimeElement.innerHTML = puertoRicoTime.format(
+      "h:mm:ss [<small>]a[</small>]"
+    );
+  }
 }
 displayTime();
 setInterval(displayTime, 1000);
